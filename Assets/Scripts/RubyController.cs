667a506c1322d332e25dd42d3e3248673de151af
AlvaroSbsn <78981404+AlvaroSbsn@users.noreply.github.com>
2021-09-21 16:25:50 +0200

    ¿Cómo se usa tu código en Unity?

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -14,7 +14,7 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.05f;
         transform.position = position;
     }
 }
