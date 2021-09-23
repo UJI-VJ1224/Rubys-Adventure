65d82a21c86aef11ac1ec6f80d6a417f041970a9
Fernando Gramage <fergr30@gmail.com>
2021-09-23 16:28:14 +0200

    Character controller through Keyboard Input

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -11,8 +11,14 @@ public class RubyController : MonoBehaviour
 
     void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        
+        position.x = position.x + 5f * horizontal * Time.deltaTime;
+        position.y = position.y + 5f * vertical * Time.deltaTime;
+
         transform.position = position;
     }
 }
