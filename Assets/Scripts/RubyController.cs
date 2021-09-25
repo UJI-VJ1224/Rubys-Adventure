08c617f275d91e4fb1a27bff37044a2a502472c6
Paumarpa <al397403@uji.es>
2021-09-26 00:14:37 +0200

    FrameIndependent movement

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -20,8 +20,8 @@ public class RubyController : MonoBehaviour
 
         Debug.Log(vertical);
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
-        position.y = position.y + 0.1f * vertical * Time.deltaTime;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
 
     }
