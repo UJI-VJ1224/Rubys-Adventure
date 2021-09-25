f71e906a3eb8fe90160c26715dca2ea9f0d4fec8
Paumarpa <al397403@uji.es>
2021-09-25 19:49:36 +0200

    Script modified

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,6 +13,9 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
+
     }
 }
