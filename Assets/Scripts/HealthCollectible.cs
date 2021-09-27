06bbdd696615b26a28388bf07f2698ac4b0f68e5
AdriMon27 <al394498@uji.es>
2021-09-28 00:50:13 +0200

    adjust the ruby controller script, check whether ruby needs health, define a property in ruby controller, use the property in healthcollectible and check my scripts

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -9,7 +9,7 @@ public class HealthCollectible : MonoBehaviour
         RubyController controller = other.GetComponent<RubyController>();
         
         if (controller != null) {
-            if (controller.currentHealth < controller.maxHealth) { 
+            if (controller.health < controller.maxHealth) { 
                 controller.ChangeHealth(1);
                 Destroy(gameObject);
             }
