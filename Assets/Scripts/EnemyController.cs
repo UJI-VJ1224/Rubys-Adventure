77cba9d2432f3533ef78c53912cb167c08e1959a
AnabelTorner <al394504@uji.es>
2021-09-30 20:03:49 +0200

    Fixing the Smoke Movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
@@ -81,5 +83,9 @@ public class EnemyController : MonoBehaviour
     {
         broken = false;
         rigidbody2D.simulated = false;
+        //optional if you added the fixed animation
+        animator.SetTrigger("Fixed");
+
+        smokeEffect.Stop();
     }
 }
