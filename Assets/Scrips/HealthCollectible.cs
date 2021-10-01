24e7c02d312be24ea83da30aabdf4de7b14c5b83
Swappppy <francft1@gmail.com>
2021-10-01 21:11:01 +0200

    Se añadieron objetos capaces de dañar a Ruby

--- Assets/Scrips/HealthCollectible.cs
+++ Assets/Scrips/HealthCollectible.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
 
         if (controller != null)
         {
-            if (controller.health < controller.maxHealth)
-            {
-                controller.ChangeHealth(1);
-                Destroy(gameObject);
-            }
+            controller.ChangeHealth(-1);
         }
     }
 }
