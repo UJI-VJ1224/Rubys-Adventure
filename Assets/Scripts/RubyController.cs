5d0bdb9c9d85c73ceb1e99184cb6f658253af191
Dastdjc <90397187+Dastdjc@users.noreply.github.com>
2021-10-03 19:51:21 +0200

    1.¿Cómo añadir vitalidad a Ruby?

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,6 +7,7 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     public int maxHealth = 5;
     int currentHealth;
+    public int speed = 10;
 
     void Start()
     {
@@ -21,9 +22,15 @@ public class RubyController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 10.0f* horizontal * Time.deltaTime;
-        position.y = position.y + 10.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
