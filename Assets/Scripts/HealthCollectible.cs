8994abc9e8126893401cf45345d5d603ebde8c0e
AlvaroSbsn <78981404+AlvaroSbsn@users.noreply.github.com>
2021-10-05 16:13:59 +0200

    fix?

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthCollectible : MonoBehaviour
-{
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        RubyController controller = other.GetComponent<RubyController>();
+//public class HealthCollectible : MonoBehaviour
+//{
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+      //  RubyController controller = other.GetComponent<RubyController>();
 
-        if (controller != null)
-        {
-            controller.ChangeHealth(1);
-            Destroy(gameObject);
-        }
-    }
-}
+        //if (controller != null)
+        //{
+          //  controller.ChangeHealth(1);
+            //Destroy(gameObject);
+        //}
+    //}
+//}
