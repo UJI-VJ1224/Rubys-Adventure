a9a7474f5377986f0615807e8416297ea2d1de67
Paumarpa <al397403@uji.es>
2021-10-07 15:53:55 +0200

    water collider

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+   Rigidbody2D rigidbody2d;
+    
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x - 10.0f* horizontal * Time.deltaTime;
+        position.y = position.y + 10.0f * vertical * Time.deltaTime;
 
-        Debug.Log(vertical);
-        Vector2 position = transform.position;
-        position.x = position.x - 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
-
+        rigidbody2d.MovePosition(position);
     }
 }
