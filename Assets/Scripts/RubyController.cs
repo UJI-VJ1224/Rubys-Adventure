1af3dd15d7be2b30da6a0077fb8761f049edbfaa
Paumarpa <al397403@uji.es>
2021-10-09 01:14:21 +0200

    Tarjeta interacion Objetos terminada

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -9,16 +9,17 @@ public class RubyController : MonoBehaviour
     public int maxHealth = 5;
 
     public int health { get { return currentHealth; } }
+
     int currentHealth;
+
     Rigidbody2D rigidbody2d;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
 
-        currentHealth = 1;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
