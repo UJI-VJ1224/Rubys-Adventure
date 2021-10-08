c16c8757ba4fa38093fb84990174e115017e94a7
Paumarpa <al397403@uji.es>
2021-10-09 01:55:33 +0200

    Damage Zone + script

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 3.0f;
 
     public int maxHealth = 5;
 
-    public int health { get { return currentHealth; } }
 
+    public int health { get { return currentHealth; } }
     int currentHealth;
 
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+
     Rigidbody2D rigidbody2d;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,17 +32,34 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        
+
         Vector2 position = rigidbody2d.position;
-        position.x = position.x - speed* horizontal * Time.deltaTime;
+        position.x = position.x - speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
